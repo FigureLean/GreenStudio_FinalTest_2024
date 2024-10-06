3b31d27e1fb049ad435bb9f3dc330f31bec4f396
潶色毛衣 <3306499718@QQ.COM>
2024-10-07 01:33:20 +0800

    a

---
+++ image/通信2401杨宇浩/水仙花.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int k,a,b,c;
+  for (k=100; k<=999;k++){
+    a=k%10; b=k/10%10; c=k/100;
+    if(a*a*a+b*b*b+c*c*==k)
+    print("水仙花数为",k)
+       }
+       return 0;
+}
