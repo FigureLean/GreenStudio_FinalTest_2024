8713294401e20eb13d3cf61dc2bf6372b5e15098
潶色毛衣 <3306499718@QQ.COM>
2024-10-07 03:13:59 +0800

    a

--- image/通信2401杨宇浩/水仙花.c
+++ image/通信2401杨宇浩/水仙花.c
@@ -2,10 +2,11 @@
 int main()
 {
   int k,a,b,c;
-  for (k=100; k<=999;k++){
+  for (k=100; k<=999;k++)
+  {
     a=k%10; b=k/10%10; c=k/100;
-    if(a*a*a+b*b*b+c*c*==k)
-    print("水仙花数为",k)
-       }
+    if (a*a*a+b*b*b+c*c*c==k)
+    printf("%d是水仙花数",k);
+  }
        return 0;
 }
